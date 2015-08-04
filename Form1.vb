bc084e5de3474147bb1337997d44eddb2355fd2c
NounVannakGitHub <nounvannakrootid@gmail.com>
2015-08-04 23:23:46 +0700

    Add new textbox
    add textbox and add Event GotFocus to switch language

---
+++ Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub TextBox1_GotFocus(sender As Object, e As EventArgs) Handles TextBox1.GotFocus
+        InputLanguage.CurrentInputLanguage = InputLanguage.InstalledInputLanguages.Item(1)
+    End Sub
+
+    Private Sub TextBox2_GotFocus(sender As Object, e As EventArgs) Handles TextBox2.GotFocus
+        InputLanguage.CurrentInputLanguage = InputLanguage.InstalledInputLanguages.Item(0)
+    End Sub
+
+    Private Sub TextBox3_GotFocus(sender As Object, e As EventArgs) Handles TextBox3.GotFocus
+        InputLanguage.CurrentInputLanguage = InputLanguage.InstalledInputLanguages.Item(1)
+    End Sub
+    
+End Class
